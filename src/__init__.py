7f080098658812e1fa7d89b3b47fbca20da78301
Mylonas Charilaos <mylonas.charilaos@gmail.com>
2022-09-18 19:03:52 +0200

    manual updates in notebook

--- src/__init__.py
+++ src/__init__.py
@@ -3,4 +3,4 @@ from .utils import _read_atoms_from_mp4, _read_table_or_header_data
 from .mp4reader import MP4Atom , MP4Data
 from .version import __version__ # created during deployment.
 
-import os
+# import os
